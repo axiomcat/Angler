4a79fa84fe3e42961336adf7ce46319974bfb96d
axiomcat <axiomcat@tuta.com>
2025-07-01 14:35:56 -0500

    Add reference source

--- angler.go
+++ angler.go
@@ -7,8 +7,10 @@ import (
 const SecondsToDays int64 = 60 * 60 * 24
 
 func GetTodayAngleIssue() int {
+	// Angle Issue and timestamp at 7am GMT-5 on July 1 2025
 	referenceIssue := 1106
 	referenceUnixTime := 1751371200
+
 	currentUnixTime := time.Now().Unix()
 	timeDiff := (currentUnixTime - int64(referenceUnixTime)) / SecondsToDays
 	currentAngleIssue := referenceIssue + int(timeDiff)
